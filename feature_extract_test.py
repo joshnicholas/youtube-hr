5f2bc2c48e74ea96cf11e5240b92bd99be427bd9
Josh Nicholas <hi@thambi.li>
2022-04-05 18:56:45 +1000

    Bunchastuff

--- feature_extract_test.py
+++ feature_extract_test.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import re 
 
+import spacy
+
+NER = spacy.load("en_core_web_lg", 
+disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
+
+# print(spacy.explain('NORP'))
 
 df = pd.read_csv('data/mit_dataset/interview_transcripts_by_turkers.csv', names=['Type', 'Transcript'])
 # 'Type', 'Transcript'
@@ -13,9 +19,14 @@ df = df.loc[~df['Type'].str.contains("pp")]
 
 # df['Transcript'] = df['Transcript'].str.split("|")
 
-df = df[:1]
+# df = df[:1]
+
+keep_ents = ['NORP','ORG', 'EVENT','PERSON','FAC','PRODUCT','WORK_OF_ART']
+
+big_stringo = ' '
 
 for index, row in df.iterrows():
+    print(index)
     transcript = row['Transcript']
     transcript = transcript.split("|")
     transcript = [x for x in transcript if "Interviewer" not in x]
@@ -26,7 +37,33 @@ for index, row in df.iterrows():
     interviewee = " ".join(transcript)
     interviewee = interviewee.replace('Interviewee:', '')
 
-    print(interests)
+    proc = NER(interviewee)
+
+    extra = [x.text for x in proc.ents if x.label_ in keep_ents]
+
+    extra = list(set(extra))
+
+    print(extra)
+
+    # big_stringo += interviewee
+
+# proc = NER(big_stringo)
+
+# refs =  [x.label_ for x in proc.ents]
+# refs = list(set(refs))
+# refs = [(x, spacy.explain(x)) for x in refs]
+
+# # reffers = 
+
+
+
+# print(refs)
+
+
+
+
+
+    # print(interests)
 
     # print(activities)
     # print(interviewee)
